74a9ecdd6a198f2c44b5cd9436190ad7db551f4a
ynzung <syj91105@sookmyung.ac.kr>
2024-07-31 00:14:18 +0900

    [Feat] 채팅 목록 초기 설정

--- gamja/src/pages/mentee/ChatListMentee.js
+++
@@ -1,7 +0,0 @@
-import React from "react";
-
-const ChatListMentee = () => {
-  return <div></div>;
-};
-
-export default ChatListMentee;
--- gamja/src/pages/mentor/ChatListMentor.js
+++
@@ -1,7 +0,0 @@
-import React from "react";
-
-const ChatListMentor = () => {
-  return <div></div>;
-};
-
-export default ChatListMentor;
