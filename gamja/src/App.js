cfd5a5889399e05b1a4b5e4ab3d6e7b215bafa27
Youbin Jang <120731400+youbing5921@users.noreply.github.com>
2024-08-01 16:25:54 +0900

    [Feat] 회원가입 프론트-백 연동

--- gamja/src/App.js
+++ gamja/src/App.js
@@ -4,6 +4,7 @@ import styled from "styled-components";
 import WelcomePage from "./pages/join/WelcomePage";
 import TermsOfServicePage from "./pages/join/TermsOfServicePage";
 import InputInfo from "./pages/join/InputInfo";
+import ChooseCategory from "./pages/join/ChooseCategory";
 import JoinComplete from "./pages/join/JoinComplete";
 import Login from "./pages/login/Login";
 import FindIdPw from "./pages/login/FindIdPw";
@@ -22,54 +23,53 @@ import MypageMentee from "./pages/mentee/MypageMentee";
 
 function App() {
   return (
-    <>
-      <Wrapper>
-        <Routes>
-          <Route path="/" element={<Login />}></Route>
-          <Route path="/join" element={<WelcomePage />}></Route>
-          <Route path="/join/tos" element={<TermsOfServicePage />}></Route>
-          <Route path="/join/info" element={<InputInfo />}></Route>
-          <Route path="/join/complete" element={<JoinComplete />}></Route>
-          <Route path="/login" element={<Login />}></Route>
-          <Route path="/login/findId" element={<FindIdPw />}></Route>
-          <Route path="/login/findPw" element={<FindIdPw />}></Route>
-          <Route path="/home" element={<Home />}></Route>
-          <Route path="/concerns" element={<ConcernsPage />}></Route>
-          <Route path="/find" element={<FindMentor />}></Route>
-          <Route
-            path="/chat-list/mentor/:username"
-            element={<ChatListMentor />}
-          ></Route>
-          <Route
-            path="/chat-list/mentee/:username"
-            element={<ChatListMentee />}
-          ></Route>
-          <Route
-            path="/chat/mentor/:roomId"
-            element={<ChatRoomMentor />}
-          ></Route>
-          <Route
-            path="/chat/mentee/:roomId"
-            element={<ChatRoomMentee />}
-          ></Route>
-          <Route
-            path="/chat-create/mentee/:roomId"
-            element={<CreateChat />}
-          ></Route>
-          <Route path="/category/mentor" element={<Category />}></Route>
-          <Route path="/category/mentee" element={<Category />}></Route>
-          <Route path="/matching" element={<Matching />}></Route>
-          <Route
-            path="/mypage/mentor/:username"
-            element={<MyPageMentor />}
-          ></Route>
-          <Route
-            path="/mypage/mentee/:username"
-            element={<MypageMentee />}
-          ></Route>
-        </Routes>
-      </Wrapper>
-    </>
+    <Wrapper>
+      <Routes>
+        <Route path="/" element={<Login />}></Route>
+        <Route path="/join" element={<WelcomePage />}></Route>
+        <Route path="/join/tos" element={<TermsOfServicePage />}></Route>
+        <Route path="/join/info" element={<InputInfo />}></Route>
+        <Route path="/join/category" element={<ChooseCategory />}></Route>
+        <Route path="/join/complete" element={<JoinComplete />}></Route>
+        <Route path="/login" element={<Login />}></Route>
+        <Route path="/login/findId" element={<FindIdPw />}></Route>
+        <Route path="/login/findPw" element={<FindIdPw />}></Route>
+        <Route path="/home" element={<Home />}></Route>
+        <Route path="/concerns" element={<ConcernsPage />}></Route>
+        <Route path="/find" element={<FindMentor />}></Route>
+        <Route path="/chat/onboard" element={<ChatOnBorad />}></Route>
+        <Route path="/chat/:roomId" element={<ChatRoom />}></Route>
+        <Route path="/chat/inbox" element={<ChatInBox />}></Route>
+        <Route
+          path="/chat-list/mentor/:username"
+          element={<ChatListMentor />}
+        ></Route>
+        <Route
+          path="/chat-list/mentee/:username"
+          element={<ChatListMentee />}
+        ></Route>
+        <Route path="/chat/mentor/:roomId" element={<ChatRoomMentor />}></Route>
+        <Route path="/chat/mentee/:roomId" element={<ChatRoomMentee />}></Route>
+        <Route
+          path="/chat-create/mentee/:roomId"
+          element={<CreateChat />}
+        ></Route>
+        <Route path="/chat/inbox" element={<ChatInBox />}></Route>
+        <Route path="/category/mentor" element={<Category />}></Route>
+        <Route path="/category/mentee" element={<Category />}></Route>
+        <Route path="/matching" element={<Matching />}></Route>
+        <Route
+          path="/mentor/mypage/:username"
+          element={<MyPageMentor />}
+        ></Route>
+        <Route
+          path="/mentee/mypage/:username"
+          element={<MypageMentee />}
+        ></Route>
+        <Route path="/community" element={<Community />}></Route>
+        <Route path="/community/:colId" element={<Column />}></Route>
+      </Routes>
+    </Wrapper>
   );
 }
 
