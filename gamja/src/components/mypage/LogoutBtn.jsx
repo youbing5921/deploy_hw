a5fc327d6c0ca60fceea0f65e6f010880347edaf
ynzung <syj91105@sookmyung.ac.kr>
2024-08-02 14:40:19 +0900

    [Feat] 로그아웃 버튼 컴포넌트 생성

---
+++ gamja/src/components/mypage/LogoutBtn.jsx
@@ -0,0 +1,36 @@
+import React from "react";
+import styled from "styled-components";
+import logoutBtn from "../../images/logoutBtn.svg";
+
+const LogoutBtn = () => {
+  return (
+    <>
+      <Button>
+        로그아웃하기
+        <Icon src={logoutBtn} alt={logoutIcon} />
+      </Button>
+    </>
+  );
+};
+
+export default LogoutBtn;
+
+const Button = styled.button`
+  border-radius: 20px;
+  background: rgba(73, 73, 73, 0.2);
+  padding: 7px 15px;
+  border: none;
+  color: #fff;
+  text-align: center;
+  font-family: Pretendard;
+  font-size: 15px;
+  font-weight: 700;
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+`;
+
+const Icon = styled.img`
+  width: 15px;
+  height: 18px;
+`;
