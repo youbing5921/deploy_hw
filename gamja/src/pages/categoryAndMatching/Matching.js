ec5165d4acc4a9abf0a8e7c98c3e348f6187b686
Youbin Jang <120731400+youbing5921@users.noreply.github.com>
2024-08-04 22:09:16 +0900

    [Feat] 자동 매칭 기능 추가

--- gamja/src/pages/categoryAndMatching/Matching.js
+++ gamja/src/pages/categoryAndMatching/Matching.js
@@ -6,77 +6,66 @@ import BackBtn from "../../components/categoryAndMatching/BackBtn";
 import TitleOval from "../../components/categoryAndMatching/TitleOval";
 import BottonBtn from "../../components/categoryAndMatching/BottonBtn";
 import MentorReco from "../../components/matching/MentorReco";
-
-const infoList = [
-  {
-    id: "1",
-    name: "가가가",
-    category: ["가치관", "사랑", "생활"],
-    count: [10, 10, 2],
-    rating: 50,
-  },
-  {
-    id: "2",
-    name: "나나나",
-    category: ["인간관계", "재테크", "가치관"],
-    count: [3, 8, 1],
-    rating: 65,
-  },
-  {
-    id: "3",
-    name: "다다다",
-    category: ["인간관계", "재테크", "가치관"],
-    count: [9, 23, 2],
-    rating: 35,
-  },
-  {
-    id: "4",
-    name: "라라라",
-    category: ["인간관계", "재테크", "진로"],
-    count: [15, 10, 20],
-    rating: 30,
-  },
-  {
-    id: "5",
-    name: "마마마",
-    category: ["인간관계", "재테크", "사랑"],
-    count: [15, 30, 20],
-    rating: 96,
-  },
-];
+import axios from "axios";
 
 const Matching = () => {
   const navigate = useNavigate();
+  const category = useLocation().state.category;
+  const [mentorInfo, setMentorInfo] = useState([]);
 
-  return (
-    <MainContainer>
-      <BackBtn onClick={() => navigate(-1)} />
-      <NewTitleOval>멘토 매칭</NewTitleOval>
-      <TitleText>
-        이지님이 찾던
-        <br />
-        멘토님을 소개해드릴게요.
-      </TitleText>
-      <MentorReco infoList={infoList}> </MentorReco>
-      <BtnDiv>
-        <NewBottonBtn
-          onClick={() => {
-            alert("관심멘토로 설정되었습니다.");
-          }}
-        >
-          관심멘토로 설정하기
-        </NewBottonBtn>
-        <NewBottonBtn1
-          onClick={() => {
-            console.log("멘토님께 채팅하기");
-          }}
-        >
-          멘토님께 채팅하기
-        </NewBottonBtn1>
-      </BtnDiv>
-      <HorizonLine text="홈으로 이동하기" onClick={() => navigate("/home")} />
-    </MainContainer>
-  );
+  useEffect(() => {
+    axios
+      .post(
+        `http://127.0.0.1:8000/matching/`,
+        {
+          interests: category,
+        },
+        {
+          headers: {
+            Authorization: `Bearer ${localStorage.getItem("access")}`,
+          },
+        }
+      )
+      .then((response) => {
+        console.log(response.data);
+        setMentorInfo(response.data);
+      })
+      .catch((error) => console.log(error));
+  }, []);
+
+  if (mentorInfo.length === 0) {
+    return <></>;
+  } else {
+    return (
+      <MainContainer>
+        <BackBtn onClick={() => navigate(-1)} />
+        <NewTitleOval>멘토 매칭</NewTitleOval>
+        <TitleText>
+          이지님이 찾던
+          <br />
+          멘토님을 소개해드릴게요.
+        </TitleText>
+        <MentorReco infoList={mentorInfo} />
+        <BtnDiv>
+          <NewBottonBtn
+            onClick={() => {
+              alert("관심멘토로 설정되었습니다.");
+            }}
+          >
+            관심멘토로 설정하기
+          </NewBottonBtn>
+          <NewBottonBtn1
+            onClick={() => {
+              console.log("멘토님께 채팅하기");
+            }}
+          >
+            멘토님께 채팅하기
+          </NewBottonBtn1>
+        </BtnDiv>
+        <HorizonLine text="홈으로 이동하기" onClick={() => navigate("/home")} />
+      </MainContainer>
+    );
+  }
 };
 
 export default Matching;
