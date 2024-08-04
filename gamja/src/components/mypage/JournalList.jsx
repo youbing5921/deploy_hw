9aeaefa5ec6b31b79530f531638000e82a4b9fac
ynzung <syj91105@sookmyung.ac.kr>
2024-08-04 19:41:51 +0900

    [Feat] 멘토멘티 멘토링 일지 목록 조회 연동

--- gamja/src/components/mypage/JournalList.jsx
+++ gamja/src/components/mypage/JournalList.jsx
@@ -1,40 +1,35 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import styled from "styled-components";
 import { useNavigate } from "react-router-dom";
 import { useLocation } from "react-router-dom";
 import { matchPath } from "react-router";
-
-const JournalInfo = [
-  {
-    id: 1,
-    title: "사랑과 돈",
-    date: "2024.07.20",
-  },
-  {
-    id: 2,
-    title: "인생을 값지게 사는 법",
-    date: "2024.06.02",
-  },
-  {
-    id: 3,
-    title: "흰 빨래 잘 하는 팁",
-    date: "2024.06.01",
-  },
-  {
-    id: 4,
-    title: "도전과 실패",
-    date: "2024.05.21",
-  },
-  {
-    id: 5,
-    title: "짝사랑의 가치",
-    date: "2024.04.12",
-  },
-];
+import axios from "axios";
 
 const JournalList = ({ txt, $fontColor, $bgColor, Info }) => {
   const location = useLocation().pathname;
   const navigate = useNavigate();
+  const [journalInfo, setJournalInfo] = useState([]);
+
+  const getJournalList = () => {
+    axios
+      .get("http://127.0.0.1:8000/log/", {
+        headers: {
+          Authorization: `Bearer ${localStorage.getItem("accessToken")}`,
+        },
+      })
+      .then((response) => {
+        console.log(response.data);
+        setJournalInfo(response.data);
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  };
+
+  useEffect(() => {
+    getJournalList();
+  }, []);
+
   if (matchPath("/mypage/mentee/:username", location)) {
     return (
       <>
@@ -51,7 +46,7 @@ const JournalList = ({ txt, $fontColor, $bgColor, Info }) => {
           </Top>
           <BoldHr />
           <ListBox>
-            {JournalInfo.map((journal) => (
+            {journalInfo.map((journal) => (
               <React.Fragment key={journal.id}>
                 <Journal onClick={() => navigate("/mypage/journal/detail/:id")}>
                   <JournalTitle>{journal.title}</JournalTitle>
@@ -62,7 +57,6 @@ const JournalList = ({ txt, $fontColor, $bgColor, Info }) => {
             ))}
           </ListBox>
         </Container>
-        ;
       </>
     );
   }
@@ -82,7 +76,7 @@ const JournalList = ({ txt, $fontColor, $bgColor, Info }) => {
           </Top>
           <BoldHr />
           <ListBox>
-            {JournalInfo.map((journal) => (
+            {journalInfo.map((journal) => (
               <React.Fragment key={journal.id}>
                 <Journal onClick={() => navigate("/mypage/journal/detail/:id")}>
                   <JournalTitle>{journal.title}</JournalTitle>
