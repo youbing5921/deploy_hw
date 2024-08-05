f0d889132a50600ad04ac759023e44dace009605
ynzung <syj91105@sookmyung.ac.kr>
2024-08-05 12:37:06 +0900

    [Chore] 멘토 찾기 목록 스타일 수정

--- gamja/src/components/mentee/MentorInfo.jsx
+++ gamja/src/components/mentee/MentorInfo.jsx
@@ -140,7 +140,8 @@ const SubscribeButton = styled.img`
 
 const MiddleBox = styled.div`
   display: flex;
-  gap: 14px;
+  align-items: center;
+  justify-content: space-between;
   margin-top: 12px;
 `;
 
