f78b7aada083c3899b100bb6a5dc0528466ca2d7
ynzung <syj91105@sookmyung.ac.kr>
2024-08-06 02:37:23 +0900

    [Fix] 상세고민조회 댓글 안보임 오류 수정

--- gamja/src/components/mypage/MyConcernList.jsx
+++ gamja/src/components/mypage/MyConcernList.jsx
@@ -8,7 +8,6 @@ const MyConcernList = () => {
   const [Info, setInfo] = useState([]);
   const navigate = useNavigate();
   const accessToken = localStorage.getItem("access");
-  const hasComments = Info?.concern?.comments?.length > 0;
 
   useEffect(() => {
     const getAllConcerns = () => {
@@ -59,9 +58,9 @@ const MyConcernList = () => {
               삭제
             </DeleteBtn>
           </TopBox>
-          {hasComments && (
+          {info?.comments?.length > 0 && (
             <ReplyWrapper>
-              {info?.comments?.map((comment, idx) => (
+              {info.comments.map((comment, idx) => (
                 <Reply
                   key={idx}
                   onClick={() => navigate(`/profile/mentor/${comment.author}`)}
@@ -95,6 +94,7 @@ const TopBox = styled.div`
   align-items: center;
   justify-content: space-between;
 `;
+
 const DeleteBtn = styled.div`
   color: #7f7f7f;
   font-size: 13px;
