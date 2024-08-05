a53a53580d3d040561b238987b16a4eb87be9549
ynzung <syj91105@sookmyung.ac.kr>
2024-08-06 00:38:40 +0900

    [Add] 고민 데이터 없을 경우 처리 코드 추가

--- gamja/src/components/mypage/MyConcern.jsx
+++ gamja/src/components/mypage/MyConcern.jsx
@@ -5,33 +5,45 @@ import { useNavigate } from "react-router-dom";
 
 const MyConcern = ({ Info }) => {
   const navigate = useNavigate();
+  const username = localStorage.getItem("name");
+  const hasConcern = Info?.concern?.content?.length > 0;
+  const hasComments = Info?.concern?.comments?.length > 0;
+
   return (
     <>
       <Container>
         <Top>
           <Title>나의 한 줄 고민</Title>
           <ButtonBox>
-            <Button onClick={() => navigate("/mypage/concerns-list/")}>
-              지난 고민 보기
-            </Button>
+            {hasConcern && (
+              <Button onClick={() => navigate("/mypage/concerns-list/")}>
+                지난 고민 보기
+              </Button>
+            )}
             <Button onClick={() => navigate("/concern/write/")}>
               고민 생성하기
             </Button>
           </ButtonBox>
         </Top>
         <Content>
-          <Concern>{Info?.concern?.content}</Concern>
-          <ReplyWrapper>
-            {Info?.concern?.comments?.map((comment, idx) => (
-              <Reply
-                key={idx}
-                onClick={() => navigate(`/profile/mentor/${comment.author}`)}
-              >
-                <Profile src={MentorImg} alt="MentorImg" />
-                <Text>{comment.content}</Text>
-              </Reply>
-            ))}
-          </ReplyWrapper>
+          <Concern>
+            {hasConcern
+              ? Info.concern.content
+              : `${username}님의 고민을 공유해 주세요`}
+          </Concern>
+          {hasConcern && hasComments && (
+            <ReplyWrapper>
+              {Info.concern.comments.map((comment, idx) => (
+                <Reply
+                  key={idx}
+                  onClick={() => navigate(`/profile/mentor/${comment.author}`)}
+                >
+                  <Profile src={MentorImg} alt="MentorImg" />
+                  <Text>{comment.content}</Text>
+                </Reply>
+              ))}
+            </ReplyWrapper>
+          )}
         </Content>
       </Container>
     </>
@@ -50,6 +62,7 @@ const Top = styled.div`
   align-items: center;
   justify-content: space-between;
 `;
+
 const Title = styled.div`
   color: #494949;
   text-align: center;
@@ -57,12 +70,14 @@ const Title = styled.div`
   font-weight: 700;
   padding: 13px 3px;
 `;
+
 const ButtonBox = styled.div`
   display: inline-flex;
   justify-content: center;
   align-items: center;
   gap: 10px;
 `;
+
 const Button = styled.button`
   height: 27px;
   padding: 0px 10px;
